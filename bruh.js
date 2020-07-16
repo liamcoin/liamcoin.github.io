d70d803bfd9ecff620e109cab92c25362abba4a3
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2020-07-16 17:41:54 +1000

    Update bruh.js

--- bruh.js
+++ bruh.js
@@ -1,33 +1,23 @@
 console.log("bruh");
-// 1. Create a new XMLHttpRequest object
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://liamcoin.locknessko.repl.co');
-xhr.send();
-// 4. This will be called after the response is received
-xhr.onload = function() {
-  if (xhr.status != 200) { // analyze HTTP status of the response
-    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-  } else { // show the result
-		document.getElementById("nc").innerHTML="Current Serial Number "+xhr.response;
-  }
-};
-xhr.onprogress = function(event) {};
-xhr.onerror = function() {
-  alert("Request failed");
-};
 
-xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://liamcoin.locknessko.repl.co/s');
-xhr.send();
-// 4. This will be called after the response is received
-xhr.onload = function() {
-  if (xhr.status != 200) { // analyze HTTP status of the response
-    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-  } else { // show the result
-		document.getElementById("ns").innerHTML="Current Serial Number "+xhr.response;
-  }
-};
-xhr.onprogress = function(event) {};
-xhr.onerror = function() {
-  alert("Request failed");
-};
+function rec(o){
+	// 1. Create a new XMLHttpRequest object
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://liamcoin.locknessko.repl.co');
+	xhr.send();
+	// 4. This will be called after the response is received
+	xhr.onload = function() {
+		if (xhr.status != 200) { // analyze HTTP status of the response
+			alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+		} else { // show the result
+			document.getElementById(o).innerHTML+=xhr.response;
+		}
+	};
+	xhr.onprogress = function(event) {};
+	xhr.onerror = function() {
+		alert("Request failed");
+	};
+}
+
+rec("nc")
+rec("ns")
