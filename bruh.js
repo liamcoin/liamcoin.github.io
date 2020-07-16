756953b0c83308cb6cdda92dd6af25e0c5daa927
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2020-07-16 17:38:31 +1000

    Update bruh.js

--- bruh.js
+++ bruh.js
@@ -1,16 +1,30 @@
 console.log("bruh");
 // 1. Create a new XMLHttpRequest object
 let xhr = new XMLHttpRequest();
-
 xhr.open('GET', 'https://liamcoin.locknessko.repl.co');
-
 xhr.send();
 // 4. This will be called after the response is received
 xhr.onload = function() {
   if (xhr.status != 200) { // analyze HTTP status of the response
     alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
   } else { // show the result
-		document.getElementById("nc").innerHTML=xhr.response;
+		document.getElementById("nc").innerHTML="Current Serial Number "+xhr.response;
+  }
+};
+xhr.onprogress = function(event) {};
+xhr.onerror = function() {
+  alert("Request failed");
+};
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://liamcoin.locknessko.repl.co/s');
+xhr.send();
+// 4. This will be called after the response is received
+xhr.onload = function() {
+  if (xhr.status != 200) { // analyze HTTP status of the response
+    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+  } else { // show the result
+		document.getElementById("ns").innerHTML="Current Serial Number "+xhr.response;
   }
 };
 xhr.onprogress = function(event) {};
